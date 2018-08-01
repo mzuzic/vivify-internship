28533c77e3f47465c3fbfacec2ddd10e11d1622c
mzuzic <marko.z@vivifyideas.com>
2018-08-01 10:22:35 +0200

    #MZ1-4 add blade rendering in HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,8 +1,19 @@
 <?php
 
-class HomeController extends Controller 
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class HomeController extends BaseController 
 {   
     public function getView() {
         return view('welcome');
+    }
+
+    public function getHello($name='') {
+        return view('hello', ['name' => $name]);
     }
 }
