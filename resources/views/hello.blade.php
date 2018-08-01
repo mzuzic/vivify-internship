86fd3c041316163e1a52f829de55e411c33a064f
mzuzic <marko.z@vivifyideas.com>
2018-08-01 10:24:43 +0200

    #MZ1-4 add blade file

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Hello</title>
+    </head>
+    <body>
+        @if ($name == '')
+            Hello
+        @else
+            Hello, {{ $name }}
+        @endif
+        
+    </body>
+<hmtl>
